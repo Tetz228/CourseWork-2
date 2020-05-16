d9ee263e36c4e343b842b608235933aa1401b47e
Кирилл Зыков <tommili652@gmail.com>
2020-05-16 09:42:15 +0700

    f

--- CourseWork/Users/UsersFormAdd.Designer.cs
+++ CourseWork/Users/UsersFormAdd.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelValidEmployee = new System.Windows.Forms.Label();
+            this.ComboBox_fk_employee = new System.Windows.Forms.ComboBox();
+            this.labelValidRole = new System.Windows.Forms.Label();
+            this.ComboBox_fk_role_user = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // labelValidEmployee
+            // 
+            this.labelValidEmployee.AutoSize = true;
+            this.labelValidEmployee.Font = new System.Drawing.Font("Verdana", 8F);
+            this.labelValidEmployee.ForeColor = System.Drawing.Color.Red;
+            this.labelValidEmployee.Location = new System.Drawing.Point(375, 178);
+            this.labelValidEmployee.Name = "labelValidEmployee";
+            this.labelValidEmployee.Size = new System.Drawing.Size(141, 13);
+            this.labelValidEmployee.TabIndex = 51;
+            this.labelValidEmployee.Text = "Некорректные данные";
+            this.labelValidEmployee.Visible = false;
+            // 
+            // ComboBox_fk_employee
+            // 
+            this.ComboBox_fk_employee.BackColor = System.Drawing.Color.DimGray;
+            this.ComboBox_fk_employee.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox_fk_employee.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ComboBox_fk_employee.ForeColor = System.Drawing.Color.White;
+            this.ComboBox_fk_employee.FormattingEnabled = true;
+            this.ComboBox_fk_employee.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.ComboBox_fk_employee.Location = new System.Drawing.Point(348, 149);
+            this.ComboBox_fk_employee.Name = "ComboBox_fk_employee";
+            this.ComboBox_fk_employee.Size = new System.Drawing.Size(189, 26);
+            this.ComboBox_fk_employee.TabIndex = 50;
+            // 
+            // labelValidRole
+            // 
+            this.labelValidRole.AutoSize = true;
+            this.labelValidRole.Font = new System.Drawing.Font("Verdana", 8F);
+            this.labelValidRole.ForeColor = System.Drawing.Color.Red;
+            this.labelValidRole.Location = new System.Drawing.Point(375, 124);
+            this.labelValidRole.Name = "labelValidRole";
+            this.labelValidRole.Size = new System.Drawing.Size(141, 13);
+            this.labelValidRole.TabIndex = 53;
+            this.labelValidRole.Text = "Некорректные данные";
+            this.labelValidRole.Visible = false;
+            // 
+            // ComboBox_fk_role_user
+            // 
+            this.ComboBox_fk_role_user.BackColor = System.Drawing.Color.DimGray;
+            this.ComboBox_fk_role_user.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox_fk_role_user.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ComboBox_fk_role_user.ForeColor = System.Drawing.Color.White;
+            this.ComboBox_fk_role_user.FormattingEnabled = true;
+            this.ComboBox_fk_role_user.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.ComboBox_fk_role_user.Location = new System.Drawing.Point(348, 95);
+            this.ComboBox_fk_role_user.Name = "ComboBox_fk_role_user";
+            this.ComboBox_fk_role_user.Size = new System.Drawing.Size(189, 26);
+            this.ComboBox_fk_role_user.TabIndex = 52;
+            // 
+            // UsersFormAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UsersFormAdd";
+            this.ClientSize = new System.Drawing.Size(549, 298);
+            this.Controls.Add(this.labelValidRole);
+            this.Controls.Add(this.ComboBox_fk_role_user);
+            this.Controls.Add(this.labelValidEmployee);
+            this.Controls.Add(this.ComboBox_fk_employee);
+            this.MaximizeBox = false;
+            this.Name = "UsersFormAdd";
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Добавить пользователя";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelValidEmployee;
+        private System.Windows.Forms.ComboBox ComboBox_fk_employee;
+        private System.Windows.Forms.Label labelValidRole;
+        private System.Windows.Forms.ComboBox ComboBox_fk_role_user;
     }
 }
--- CourseWork/Users/UsersFormAdd.cs
+++ CourseWork/Users/UsersFormAdd.cs
@@ -1,20 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MaterialSkin.Controls;
+using MaterialSkin;
 using System.Windows.Forms;
 
 namespace CourseWork.Users
 {
-    public partial class UsersFormAdd : Form
+    public partial class UsersFormAdd : MaterialForm
     {
         public UsersFormAdd()
         {
             InitializeComponent();
+
+            var material = MaterialSkinManager.Instance;
+
+            material.AddFormToManage(this);
+            material.Theme = MaterialSkinManager.Themes.DARK;
+            material.ColorScheme = new ColorScheme(Primary.Orange900, Primary.Orange800, Primary.Orange400, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        // Заполнение ComboBox`а "Сотрудник"
+        private void SelectEmployeeComboBox()
+        {
+            ConnectionDB connection = new ConnectionDB();
+            SqlDataAdapter sqlDA = new SqlDataAdapter("SELECT id_employee AS Id, CONCAT(employee_lname, ' ', LEFT(employee_fname,1), '. ', IIF(employee_mname != 'Не указано', LEFT(employee_mname,1) + '. ', '- '), ' ', Email) AS Employee FROM Employees", connection.GetSqlConnect());
+            DataTable EmployeeTableComboBox = new DataTable();
+
+            connection.OpenConnect();
+
+            sqlDA.Fill(EmployeeTableComboBox);
+
+            ComboBox_fk_employee.ValueMember = "Id";
+            ComboBox_fk_employee.DisplayMember = "Employee";
+
+            DataRow row = EmployeeTableComboBox.NewRow();
+            row[0] = 0;
+            row[1] = "Сотрудник";
+            EmployeeTableComboBox.Rows.InsertAt(row, 0);
+
+            ComboBox_fk_employee.DataSource = EmployeeTableComboBox;
+
+            connection.CloseConnect();
+        }
+
+        // Заполнение ComboBox`а "Роль сотрудника"
+        private void SelectRoleComboBox()
+        {
+            ConnectionDB connection = new ConnectionDB();
+            SqlDataAdapter sqlDA = new SqlDataAdapter("SELECT id_user_role AS Id, user_name_role AS Role FROM Users_roles", connection.GetSqlConnect());
+            DataTable RoleTableComboBox = new DataTable();
+
+            connection.OpenConnect();
+
+            sqlDA.Fill(RoleTableComboBox);
+
+            ComboBox_fk_role_user.ValueMember = "Id";
+            ComboBox_fk_role_user.DisplayMember = "Role";
+
+            DataRow row = RoleTableComboBox.NewRow();
+            row[0] = 0;
+            row[1] = "Роль сотрудника";
+            RoleTableComboBox.Rows.InsertAt(row, 0);
+
+            ComboBox_fk_role_user.DataSource = RoleTableComboBox;
+
+            connection.CloseConnect();
+        }
+
+        // Проверки на пустоту полей
+        private bool CheckTextBox()
+        {
+            int check = 0;
+
+            if (string.IsNullOrWhiteSpace(textBoxProject_name.Text))
+            {
+                labelValidProject.Show();
+
+                check = 1;
+            }
+            if (ComboBox_fk_employee.Text == "Сотрудник")
+            {
+                labelValidRole.Show();
+
+                check = 1;
+            }
+            if (ComboBox_fk_role_user.Text == "Роль сотрудника")
+            {
+                labelValidEmployee.Show();
+
+                check = 1;
+            }
+
+            if (check == 1)
+                return false;
+            else
+                return true;
         }
     }
 }
